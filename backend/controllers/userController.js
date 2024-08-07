8950c355a4827e3889838aadc1eda3bbafc30a03
James Espinosa <de.james013@gmail.com>
2024-08-07 19:44:43 +0800

    errorMiddleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,10 @@
+import asyncHandler from 'express-async-handler';
+
 const authenticate = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: 'Authenticate',
-  });
+  res.status(418);
+  throw new Error("I'm a teapot");
 });
-const register = async (req, res) => {};
-const logout = async (req, res) => {};
+const register = asyncHandler(async (req, res) => {});
+const logout = asyncHandler(async (req, res) => {});
 
 export { authenticate, logout, register };
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import * as err from './middleware/errorMiddleware.js';
 import router from './routes/index.js';
 
 dotenv.config();
@@ -8,6 +9,9 @@ const app = express();
 
 app.use(express.json());
 app.use('/api', router);
+
+app.use(err.notFound);
+app.use(err.errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
