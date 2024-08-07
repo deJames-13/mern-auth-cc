8daa8df7aabdd6fd2c0c6f680effd84a3e334171
James Espinosa <de.james013@gmail.com>
2024-08-07 20:43:19 +0800

    connect to user routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,38 @@
 import asyncHandler from 'express-async-handler';
 
+// @desc    Authenticate user & get token
+// route    POST /api/users/authenticate
+// @access  Public
 const authenticate = asyncHandler(async (req, res) => {
-  res.status(418);
-  throw new Error("I'm a teapot");
+  return res.status(200).json({ message: 'Authenticated' });
 });
-const register = asyncHandler(async (req, res) => {});
-const logout = asyncHandler(async (req, res) => {});
 
-export { authenticate, logout, register };
+// @desc    Register a new user
+// route    POST /api/users
+// @access  Public
+const register = asyncHandler(async (req, res) => {
+  return res.status(201).json({ message: 'Registered' });
+});
+
+// @desc    Log user out
+// route    POST /api/users/logout
+// @access  Public
+const logout = asyncHandler(async (req, res) => {
+  return res.status(200).json({ message: 'Logged out' });
+});
+
+// @desc    Log user out
+// route    GET /api/users/profile
+// @access  Private
+const getProfile = asyncHandler(async (req, res) => {
+  return res.status(200).json({ message: 'Profile' });
+});
+
+// @desc    Log user out
+// route    PUT /api/users/profile
+// @access  Private
+const updateProfile = asyncHandler(async (req, res) => {
+  return res.status(200).json({ message: 'Profile updated' });
+});
+
+export { authenticate, getProfile, logout, register, updateProfile };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,8 +3,13 @@ import * as userController from '../controllers/userController.js';
 
 const userRouter = Router();
 
+userRouter
+  .route('/profile')
+  .get(userController.getProfile)
+  .put(userController.updateProfile);
+
+userRouter.post('/', userController.register);
 userRouter.post('/authenticate', userController.authenticate);
-userRouter.post('/register', userController.register);
 userRouter.post('/logout', userController.logout);
 
 export default userRouter;
