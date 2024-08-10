27b55c40a1d89527e895b682dcea79211c0d5bfc
James Espinosa <de.james013@gmail.com>
2024-08-10 14:48:56 +0800

    test production

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import cookieParser from 'cookie-parser';
 import express from 'express';
+import path from 'path';
 import { connectDB } from './config/index.js';
 import { MONGO_URI, PORT } from './env/index.js';
 import * as err from './middleware/errorMiddleware.js';
@@ -12,6 +13,21 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 app.use('/api', router);
+
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  const root_folder = path.join(__dirname, '../');
+
+  app.use(express.static(path.join(root_folder, 'frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(root_folder, 'frontend/dist/index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
 
 app.use(err.notFound);
 app.use(err.errorHandler);
