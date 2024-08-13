748f0dcdfb1ef7c96117bb939c9ccff9bd67e8b0
deJames <109223910+deJames-13@users.noreply.github.com>
2024-08-13 21:03:22 +0800

    adsf

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -25,7 +25,7 @@ const User = new Model({
 User.statics.fillables = ['name', 'email', 'password'];
 User.statics.hidden = ['password'];
 
-User.statics.hashPassword = async function (password) {
+User.methods.hashPassword = async function (password) {
   const salt = await bcrypt.genSalt(10);
   return await bcrypt.hash(password, salt);
 };
